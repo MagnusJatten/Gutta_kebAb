0be87cb1b3b6896c3f10b4bf51c14ad51db7004f
MagnusJatten <magnujaa@stud.ntnu.ntnu>
2025-10-19 19:46:58 +0200

    fiksa logikken i moment og skjærkraft

--- skjærkraft.py
+++ skjærkraft.py
@@ -14,35 +14,34 @@ def skjær(nelem, lastdata, moment, elemlen):
         Q_verdier[i,0] = (moment[i,0] + moment[i,1]) / L #Ende 1
         Q_verdier[i,1] = Q_verdier[i,0]                  #Ende 2
 
-        #Bidrag fra ytre laster
-        if i in lastdata[:, 0]:
-            #Punktlast 
-            if lastdata[i,4] == 1: 
-                P = lastdata[i,1]  
-                a = lastdata[i,4]
-                if a != 0 and a != L: #Punktlast ikke i bjelkeende
+        # Bidrag fra ytre laster
+        ytre_last = np.where(lastdata[:,0] == i)[0]  #Alle ytre laster på elementet
+        for idx in ytre_last:
+            # Punktlast 
+            if lastdata[idx,3] == 1: 
+                P = lastdata[idx,1]  
+                a = lastdata[idx,4]
+                if a != 0 and a != L:  # Punktlast ikke i bjelkeende
                     Q_verdier[i,1] += P*a / L
                     Q_verdier[i,0] += P - Q_verdier[i,1]
 
-            #Fordelt last       
-            elif lastdata[i,4] == 2: 
-                q1, q2 = lastdata[i,1], lastdata[i,2]
+            # Fordelt last       
+            elif lastdata[idx,3] == 2: 
+                q1, q2 = lastdata[idx,1], lastdata[idx,2]
 
-                #Bidrag fra jevnt fordelt last
+                # Bidrag fra jevnt fordelt last
                 Q_verdier[i,0] += min(q1,q2)*L/2
                 Q_verdier[i,1] -= min(q1,q2)*L/2
 
-                #Bidrag fra lineært fordelt last 
-                if max(q1,q2) == q1: #maks i ende 1
-                    q = q1 -q2 
+                # Bidrag fra lineært fordelt last 
+                if max(q1,q2) == q1:  # maks i ende 1
+                    q = q1 - q2 
                     Q_verdier[i,0] += q*L/3 
                     Q_verdier[i,1] -= q*L/6
-
-                else: #maks i ende 2
+                else:  # maks i ende 2
                     q = q2 - q1
                     Q_verdier[i,0] += q*L/6
                     Q_verdier[i,1] -= q*L/3
-        
     return Q_verdier
 
 
